86172701ebeb7775705e3d7a853e0db31f59b47f
Nilotpal Deb <juug25btech12166@jainuniversity.ac.in>
2025-11-30 19:23:14 +0530

    as name suggests

---
+++ 01-Basics/C_to_Kelvin_fahrenheit.py
@@ -0,0 +1,5 @@
+celcius=float(input("enter temperature in celcius:  "))
+kelvin=celcius+273.15
+fahrenheit=(celcius*9/5)+32
+print("the temperature in kelvin is: ",kelvin)
+print("the temperature in fahrenheit is: ",fahrenheit)
